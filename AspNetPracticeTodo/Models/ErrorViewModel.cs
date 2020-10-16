c1214f902fcf5443fe7bbeaa84df214c46ee63fa
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-10-16 16:29:12 +0200

    Initial commit

---
+++ AspNetPracticeTodo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetPracticeTodo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
