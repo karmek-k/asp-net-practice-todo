b1abcc99cda88679a7edf2d59769a2df26839d6b
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-10-16 17:02:46 +0200

    Add models

---
+++ AspNetPracticeTodo/Models/TodoList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetPracticeTodo.Models
+{
+    public class TodoList
+    {
+        public int TodoListId { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public List<TodoItem> TodoItems { get; set; }
+    }
+}
