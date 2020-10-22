f17689df74863cc61078ac62e7010e68c80dcd5e
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-10-22 18:39:52 +0200

    Create the Index action in ListController

---
+++ AspNetPracticeTodo/Controllers/ListController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetPracticeTodo.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetPracticeTodo.Controllers
+{
+    public class ListController : Controller
+    {
+        private readonly TodoDbContext _db;
+
+        public ListController(TodoDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var todoList = _db.TodoLists
+                .Include("TodoItems")
+                .Where(list => list.TodoListId == id)
+                .FirstOrDefault();
+
+            if (todoList == null)
+            {
+                return RedirectToAction("Index", "HomeCOntroller");
+            }
+
+            return View(todoList);
+        }
+    }
+}
