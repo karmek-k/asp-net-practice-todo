65da9408f63292dc4727019ef51eefdb917c2727
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-10-22 17:27:20 +0200

    Delete unneccessary usings in HomeController.cs

--- AspNetPracticeTodo/Controllers/HomeController.cs
+++ AspNetPracticeTodo/Controllers/HomeController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AspNetPracticeTodo.Models;
 using AspNetPracticeTodo.Data;
-using Microsoft.EntityFrameworkCore.Internal;
-using System.Text;
 
 namespace AspNetPracticeTodo.Controllers
 {
