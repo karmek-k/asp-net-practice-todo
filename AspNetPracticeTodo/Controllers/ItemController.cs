d470bc2fa23b0bf7764d7c6d65fbad6efdc042d3
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-10-26 20:14:30 +0100

    Create ItemController and inject db into it

---
+++ AspNetPracticeTodo/Controllers/ItemController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetPracticeTodo.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetPracticeTodo.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly TodoDbContext _db;
+
+        public ItemController(TodoDbContext db)
+        {
+            _db = db;
+        }
+    }
+}
